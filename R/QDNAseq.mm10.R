3d77dcf9e8fc1c05330c357405c702950c082de3
Daoud Sie <daoud.sie@gmail.com>
2015-09-22 16:40:58 +0200

    Updated QDNAseq.mm10.R
    Added Rdoc-style documentation for each dataset

--- R/QDNAseq.mm10.R
+++ R/QDNAseq.mm10.R
@@ -1,21 +1,191 @@
 #########################################################################/**
 # @RdocData mm10.1000kbp.SR50
 #
-# @title "mm10 1000kbp bin annotations"
+# @title "Mm10 1000kbp bin annotations"
 # 
 # \description{ The annotated data frame contains pre calculated data for
 # each bin describing:
-#   - chromosomal coordinate  
-#   - mappability
-#   - residual
-#   - GC content & N content
-#   - encode blacklist
-#   - DGV consensus blacklist
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
 # }
 #
 # @author "DS"
 #*/#########################################################################
 
+#########################################################################/**
+# @RdocData mm10.100kbp.SR50
+#
+# @title "Mm10 100kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData mm10.10kbp.SR50
+#
+# @title "Mm10 10kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData mm10.15kbp.SR50
+#
+# @title "Mm10 15kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData mm10.1kbp.SR50
+#
+# @title "Mm10 1kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData mm10.30kbp.SR50
+#
+# @title "Mm10 30kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData mm10.500kbp.SR50
+#
+# @title "Mm10 500kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData mm10.50kbp.SR50
+#
+# @title "Mm10 50kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
+
+#########################################################################/**
+# @RdocData mm10.5kbp.SR50
+#
+# @title "Mm10 5kbp bin annotations"
+# 
+# \description{ The annotated data frame contains pre calculated data for
+# each bin describing:
+#   * chromosome: Chromosome name,
+#   * start: Base pair start position,
+#   * end: Base pair end position,
+#   * bases: Percentage of non-N nucleotides (of full bin size),
+#   * gc: Percentage of C and G nucleotides (of non-N nucleotides),
+#   * mappability: Average mappability of 50mers with a maximum of 2 mismatches,
+#   * blacklist: Percent overlap with ENCODE blacklisted regions,
+#   * residual: Median loess residual from 1000 Genomes (50mers),
+#   * use: Whether the bin should be used in subsequent analysis steps),
+# }
+#
+# @author "DS"
+#*/#########################################################################
 
 # library(QDNAseq)
 # library(Biobase)
